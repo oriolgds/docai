c52da5490ec6789e86fbd31c4d4aa3411cd2229e
Oriol <oriolginger2008@gmail.com>
2025-09-20 23:26:20 +0200

    Add release signing config to Android build
    Configured a new release signing configuration in build.gradle.kts and updated the release build type to use it instead of the debug signing config.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -44,9 +53,7 @@ android {
     }
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
